33dc9193b5028be012a9c2d17bd0a21bc131c996
DURGESH PAL <michaeldurgesh@gmail.com>
2021-10-01 02:07:16 +0530

    code updated

--- nth_fibonaci_number.cpp
+++ nth_fibonaci_number.cpp
@@ -1,20 +1,16 @@
-#include<iostream>
-using namespace std;
-
-int fib(int n)
-{
-    if(n == 0)
-        return 0;
-    if(n == 1)
-        return 1;
-    return fib(n-1)+fib(n-2);
-}
-
-int main()
-{
-    int n, num=0;
-    cout<<"Enter the no. of term you want in fibonaci series: ";
-    cin>>n;
-    num = fib(n);
-    cout<<"Term is: "<<num;
-}
+#include <iostream>  
+using namespace std;  
+int main() {  
+  int n1=0,n2=1,n3,i,number;    
+ cout<<"Enter the number of elements: ";    
+ cin>>number;    
+ cout<<n1<<" "<<n2<<" "; //printing 0 and 1    
+ for(i=2;i<number;++i) //loop starts from 2 because 0 and 1 are already printed    
+ {    
+  n3=n1+n2;    
+  cout<<n3<<" ";    
+  n1=n2;    
+  n2=n3;    
+ }    
+   return 0;  
+   }  
